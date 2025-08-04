d1520f49c3ceb5ed40f47808226ba59cfd4b1900
sinsydev <sinsyfrontenddeveloper@gmail.com>
2025-08-04 11:54:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-     tailwindcss(),
-     react()
-    ],
+  base: '/React-Pricing-Card/', // 👈 MUST match GitHub repo name exactly
+  plugins: [tailwindcss(), react()],
 })
+
